e938445dde6f3d0ba04faf6b1acfa07abf7c6de3
GGQQmaxweb <145407967+GGQQmaxweb@users.noreply.github.com>
2025-12-02 11:43:49 +0800

    feat: Enhance CLI output formatting for todos and courses, adding table display

--- cli.py
+++ cli.py
@@ -8,21 +8,59 @@ async def handle_cli(args, api):
 
     if args.todo:
         result = await api.get_todos()
-        print(result)
+        if args.raw:
+            print(result)
+            return
+        table = Texttable()
+        table.set_deco(Texttable.HEADER)
+        table.set_cols_align(["l", "l", "l", "l"])
+        table.set_cols_valign(["m", "m", "m", "m"])
+
+        # Header row
+        table.add_rows([
+            ["Course", "Title", "Due", "Id"]
+        ])
+
+        todolist = result.get("todo_list", [])
+
+        for todo in todolist:
+            course_name = todo.get("course_name", "").strip()
+            title = todo.get("title", "")
+            due = todo.get("end_time", "")
+            id = todo.get("id", "")
+
+            table.add_row([course_name, title, due,id])
+        print(table.draw())
         return
 
     if args.courses:
         result = await api.get_courses()
-        print(result)
+        if args.raw:
+            print(result)
+            return
+        table = Texttable()
+        table.set_deco(Texttable.HEADER)
+        table.set_cols_align(["l", "l", "l"])
+        table.set_cols_valign(["m", "m", "m"])
+
+        # Header row
+        table.add_rows([
+            ["Course", "Title", "Due"]
+        ])
+
+        Courses = result.get("courses", [])
+
+        for Course in Courses:
+            course_name = Course.get("name", "").strip()
+            title = Course.get("title", "")
+            
+            table.add_row([course_name, title, due])
+        print(table.draw())
         return
 
     if args.upload:
         uploaded_id = await api.upload_file(args.upload)
         print("Uploaded:", uploaded_id)
-        return
-
-    if args.nofullui:
-        print("no full UI mode triggered")
         return
 
     print("No CLI command matched. Use --help.")
